5cd14839f8409ae31e37b02264682ae7c18c5261
Whatar <leonardo.bonadimani@filosoft.it>
2024-11-25 09:12:12 -0500

    added missing routes

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,8 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    """return all pictures"""
+    return jsonify(data), 200
 
 ######################################################################
 # GET A PICTURE
@@ -44,15 +45,25 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    """return a picture by id"""
+    picture = [picture for picture in data if picture["id"] == id]
+    if picture:
+        return jsonify(picture[0]), 200
 
+    return {"message": "Picture not found"}, 404
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    """create a picture"""
+    picture = request.get_json()
+    if any(p["id"] == picture["id"] for p in data):
+        return jsonify({"Message": f"picture with id {picture['id']} already present"}), 302
+
+    data.append(picture)
+    return jsonify(picture), 201
 
 ######################################################################
 # UPDATE A PICTURE
@@ -61,11 +72,24 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    """update a picture"""
+    picture = request.get_json()
+    for i, p in enumerate(data):
+        if p["id"] == id:
+            data[i] = picture
+            return jsonify(picture), 201
+
+    return jsonify({"Message": "picture not found"}), 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    """delete a picture"""
+    for i, p in enumerate(data):
+        if p["id"] == id:
+            data.pop(i)
+            return jsonify({"message": "picture deleted"}), 204
+
+    return jsonify({"Message": "picture not found"}), 404
